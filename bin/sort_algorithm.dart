9347c014cd8e147e7dbfccf012d6f74a9dfb4955
antonionicolau <antonio.tioypedro1234@gmail.com>
2023-06-08 02:24:49 +0100

    refactor: added all algorithms to main

--- bin/sort_algorithm.dart
+++ bin/sort_algorithm.dart
@@ -9,11 +9,16 @@ void main(List<String> arguments) {
   final unsortedList = [60, 3, 10, 5, 70, 12, 2, 56, 42, 12, 0];
   // List<int> unsortedList = List<int>.generate(500, (_) => Random().nextInt(1000));
 
-  // final newList = bubbleSort(listToSort);
-  // final newList = quickSort(listToSort, 0, listToSort.length - 1);
-  // final newList = mergeSort(unsortedList, 0, unsortedList.length - 1);
-  // final newList = selectionSort(unsortedList);
-  final newList = insertionSort(unsortedList);
+  final bubbleSortList = bubbleSort(unsortedList);
+  final quickSortList = quickSort(unsortedList, 0, unsortedList.length - 1);
+  final mergeSortList = mergeSort(unsortedList, 0, unsortedList.length - 1);
+  final selectionSortList = selectionSort(unsortedList);
+  final insertionSortList = insertionSort(unsortedList);
 
-  print(newList);
+  print('List: $unsortedList\n');
+  print('Bubble Sort: $bubbleSortList\n');
+  print('Quick Sort: $quickSortList\n');
+  print('Merge Sort: $mergeSortList\n');
+  print('Selection Sort: $selectionSortList\n');
+  print('Insertion Sort: $insertionSortList\n');
 }
