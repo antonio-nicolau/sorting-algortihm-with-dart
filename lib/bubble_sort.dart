5578f1419d0770e666aef193105881bab68475c3
antonionicolau <antonio.tioypedro1234@gmail.com>
2023-06-09 01:59:55 +0100

    doc: added bubbleSort comments

--- lib/bubble_sort.dart
+++ lib/bubble_sort.dart
@@ -1,13 +1,27 @@
-List<int> bubbleSort(List<int> list) {
-  for (var i = 0; i < list.length; i++) {
-    for (var j = i + 1; j < list.length; j++) {
-      if (list[i] > list[j]) {
-        final temp = list[i];
-        list[i] = list[j];
-        list[j] = temp;
+void swap(List<int> values, int left, int right) {
+  final temp = values[left];
+  values[left] = values[right];
+  values[right] = temp;
+}
+
+List<int> bubbleSort(List<int> values) {
+  // Optimize the algorithm by stopping the algorithm
+  // if the inner loop didn’t cause any swap.
+  bool swapped = false;
+
+  for (var i = 0; i < values.length; i++) {
+    swapped = false;
+
+    for (var j = i + 1; j < values.length; j++) {
+      if (values[i] > values[j]) {
+        swap(values, i, j);
+        swapped = true;
       }
     }
+
+    // If no two elements were swapped by inner loop, then break
+    if (!swapped) break;
   }
 
-  return list;
+  return values;
 }
